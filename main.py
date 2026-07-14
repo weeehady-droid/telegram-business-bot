aee1dde1fb7430478cb2cc9df82acfe37d22146a
weeehady-droid <weeehady@gmail.com>
2026-07-14 07:34:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
 
+TOKEN = os.environ.get("BOT_TOKEN")
+
 replies = {
-    "السلام": "وعليكم السلام",
-    "السعر": "السعر 100 جنيه",
-    "العنوان": "القاهرة"
+    "سعر": "سعر الاشتراك 100 جنيه",
+    "واتس": "01000000000",
+    "عنوان": "القاهرة"
 }
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return "Bot is running!"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+
+    print(data)
+
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
