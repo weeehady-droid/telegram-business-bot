adb953c4eb55570e752bb73365af65bef3fb6370
weeehady-droid <weeehady@gmail.com>
2026-07-15 03:39:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,16 @@ import requests
 import os
 
 app = Flask(__name__)
-
 TOKEN = os.environ["BOT_TOKEN"]
 OWNER_ID = 5260085571
 
 replies = {
-    "usdt": """<blockquote>Enter and click on any wallet to be copied <tg-emoji emoji-id="5332668748044204575"></tg-emoji></blockquote>
-
-<blockquote>- Binance <tg-emoji emoji-id="5420232672964275159"></tg-emoji> : (1156755586)</blockquote>
-
-<blockquote>- Bybit <tg-emoji emoji-id="5433900293987261516"></tg-emoji> : (523496990)</blockquote>
-
-<blockquote><tg-emoji emoji-id="5832251986635920010"></tg-emoji> Don't forget to take a screenshot
+    "usdt": """<blockquote>Enter and click on any wallet to be copied <tg-emoji emoji-id="5332668748044204575">👆</tg-emoji></blockquote>
+<blockquote>- Binance <tg-emoji emoji-id="5420232672964275159">🟡</tg-emoji> : (1156755586)</blockquote>
+<blockquote>- Bybit <tg-emoji emoji-id="5433900293987261516">🟠</tg-emoji> : (523496990)</blockquote>
+<blockquote><tg-emoji emoji-id="5832251986635920010">📸</tg-emoji> Don't forget to take a screenshot
 of the transaction you made.</blockquote>"""
 }
-
 
 def send(chat_id, text, business_connection_id=None):
     payload = {
@@ -25,22 +20,17 @@ def send(chat_id, text, business_connection_id=None):
         "text": text,
         "parse_mode": "HTML"
     }
-
     if business_connection_id:
         payload["business_connection_id"] = business_connection_id
-
     r = requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
         json=payload
     )
-
     print(r.text)
-
 
 @app.route("/")
 def home():
     return "Bot is running!"
-
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
@@ -48,36 +38,27 @@ def webhook():
     print(data)
 
     if "business_message" in data:
-
         msg = data["business_message"]
-
         # يرد على رسائلك أنت فقط
         if msg.get("from", {}).get("id") != OWNER_ID:
             return "OK", 200
-
         text = msg.get("text", "").strip().lower()
         chat_id = msg["chat"]["id"]
         business_connection_id = msg["business_connection_id"]
-
         if text in replies:
             send(chat_id, replies[text], business_connection_id)
 
     elif "message" in data:
-
         msg = data["message"]
-
         # يرد على رسائلك أنت فقط
         if msg.get("from", {}).get("id") != OWNER_ID:
             return "OK", 200
-
         text = msg.get("text", "").strip().lower()
         chat_id = msg["chat"]["id"]
-
         if text in replies:
             send(chat_id, replies[text])
 
     return "OK", 200
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
